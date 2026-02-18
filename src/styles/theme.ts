607bea05f46bf3dfdf800fe046ebd4050985c12e
hanheel <hanhee630630@gmail.com>
2026-02-18 19:54:51 +0900

    feat: 폰트 디자인시스템 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,64 @@
+export const theme = {
+  typography: {
+    title1: {
+      fontWeight: 700,
+      fontSize: "24px",
+      lineHeight: "120%",
+      letterSpacing: "-0.02em",
+    },
+    heading1: {
+      fontWeight: 700,
+      fontSize: "20px",
+      lineHeight: "130%",
+      letterSpacing: "-0.02em",
+    },
+    heading2: {
+      fontWeight: 600,
+      fontSize: "18px",
+      lineHeight: "130%",
+      letterSpacing: "-0.02em",
+    },
+    heading3: {
+      fontWeight: 400,
+      fontSize: "18px",
+      lineHeight: "130%",
+      letterSpacing: "-0.02em",
+    },
+    body1: {
+      fontWeight: 700,
+      fontSize: "16px",
+      lineHeight: "160%",
+      letterSpacing: "-0.02em",
+    },
+    body2: {
+      fontWeight: 400,
+      fontSize: "16px",
+      lineHeight: "160%",
+      letterSpacing: "-0.02em",
+    },
+    subBody: {
+      fontWeight: 500,
+      fontSize: "14px",
+      lineHeight: "160%",
+      letterSpacing: "-0.02em",
+    },
+    button: {
+      fontWeight: 700,
+      fontSize: "16px",
+      lineHeight: "160%",
+      letterSpacing: "-0.02em",
+    },
+    label: {
+      fontWeight: 600,
+      fontSize: "14px",
+      lineHeight: "160%",
+      letterSpacing: "-0.02em",
+    },
+    caption: {
+      fontWeight: 400,
+      fontSize: "12px",
+      lineHeight: "160%",
+      letterSpacing: "-0.02em",
+    },
+  },
+} as const;
