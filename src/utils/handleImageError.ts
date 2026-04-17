fdb68cfb2e014a39273b2799415557d22960c6ff
hanheel <hanhee630630@gmail.com>
2026-04-18 07:40:45 +0900

    fix: 이미지 오류 핸들러 무한루프 방지 처리 추가

--- src/utils/handleImageError.ts
+++ src/utils/handleImageError.ts
@@ -2,5 +2,6 @@ export const handleImageError = (
   e: React.SyntheticEvent<HTMLImageElement, Event>,
   fallbackImageSrc: string,
 ) => {
+  e.currentTarget.onerror = null;
   e.currentTarget.src = fallbackImageSrc;
 };
