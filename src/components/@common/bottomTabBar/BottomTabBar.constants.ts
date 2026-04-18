a818ebd67fd71956f2bc44c44e1149ec1eea7077
H0ngJu <hongju4385@gmail.com>
2026-04-18 10:05:47 +0900

    refactor: TAB_ITEMS를 as const satisfies로 변경해 TabType 리터럴 추론 보장
    TabItemData[] 어노테이션이 id를 string으로 넓혀 TabType이 리터럴 유니온이
    아닌 string으로 추론되던 문제를 수정. as const satisfies 패턴으로 형태는
    검증하되 리터럴 타입은 보존하도록 변경.

--- src/components/@common/bottomTabBar/BottomTabBar.constants.ts
+++ src/components/@common/bottomTabBar/BottomTabBar.constants.ts
@@ -3,16 +3,15 @@ import IcFolder from "../../../assets/ic_folder.svg?react";
 import IcHome from "../../../assets/ic_home.svg?react";
 import IcPerson from "../../../assets/ic_person.svg?react";
 
-export interface TabItemData {
-  id: string;
-  label: string;
-  Icon: ComponentType;
-}
-
-export const TAB_ITEMS: TabItemData[] = [
+export const TAB_ITEMS = [
   { id: "홈", label: "홈", Icon: IcHome },
   { id: "방명록", label: "방명록", Icon: IcFolder },
   { id: "마이", label: "마이", Icon: IcPerson },
-];
+] as const satisfies ReadonlyArray<{
+  id: string;
+  label: string;
+  Icon: ComponentType;
+}>;
 
 export type TabType = (typeof TAB_ITEMS)[number]["id"];
+export type TabItemData = (typeof TAB_ITEMS)[number];
