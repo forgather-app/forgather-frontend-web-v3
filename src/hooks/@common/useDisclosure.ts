f46582c8098aa197484872c43df3d3711d054e86
hanheel <hanhee630630@gmail.com>
2026-04-18 16:35:57 +0900

    feat: useDisclosure 훅 추가

---
+++ src/hooks/@common/useDisclosure.ts
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+interface UseDisclosureProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const useDisclosure = ({ isOpen, onClose }: UseDisclosureProps) => {
+  const [isVisible, setIsVisible] = useState(isOpen);
+  const toggleIsVisible = () => {
+    setIsVisible(false);
+  };
+
+  const alertAnimationEnd = () => {
+    // NOTE: 바텀시트가 꺼질 떄만 동작
+    if (!isVisible) {
+      onClose();
+    }
+  };
+
+  return { isVisible, toggleIsVisible, alertAnimationEnd };
+};
+
+export default useDisclosure;
