e518e127774f76cb48fe05dd5c461669ef8c7179
hanheel <hanhee630630@gmail.com>
2026-04-18 09:55:09 +0900

    feat: BottomSheetList 컴포넌트 구현
    - 제목만 표시하는 기본형과 아이콘+설명 포함 형태를 discriminated union으로 분기
    - hover/active 상태별 배경색 변경
    - title, description 말줄임(...) 처리

---
+++ src/components/@common/BottomSheet/List/BottomSheetList.tsx
@@ -0,0 +1,46 @@
+import * as S from "./BottomSheetList.styles";
+
+type CommonProps = {
+  /** 클릭 핸들러 */
+  onClick?: () => void;
+};
+
+type BottomSheetListProps = CommonProps &
+  (
+    | {
+        /** 목록 항목 제목 */
+        title: string;
+        /** 제목 왼쪽에 표시할 아이콘 */
+        icon: React.ReactNode;
+        /** 제목 아래에 표시할 설명 */
+        description: string;
+      }
+    | {
+        /** 목록 항목 제목 */
+        title: string;
+        icon?: never;
+        description?: never;
+      }
+  );
+
+const BottomSheetList = ({
+  title,
+  icon,
+  description,
+  onClick,
+}: BottomSheetListProps) => {
+  return (
+    <S.Container
+      onClick={onClick}
+      aria-label={description ? `${title}, ${description}` : title}
+    >
+      <S.TitleRow>
+        {icon && <S.IconWrapper aria-hidden="true">{icon}</S.IconWrapper>}
+        <S.Title>{title}</S.Title>
+      </S.TitleRow>
+      {description && <S.Description>{description}</S.Description>}
+    </S.Container>
+  );
+};
+
+export default BottomSheetList;
