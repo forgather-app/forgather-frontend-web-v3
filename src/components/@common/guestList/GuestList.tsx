54a52f10edd8c800308038ef4daae13681eb31e1
hanheel <hanhee630630@gmail.com>
2026-05-09 10:52:43 +0900

    refactor: GuestListProps discriminated union으로 분리

--- src/components/@common/guestList/GuestList.tsx
+++ src/components/@common/guestList/GuestList.tsx
@@ -3,23 +3,26 @@ import IcPerson from "@/assets/icons/ic_person.svg?react";
 import IcSmallLogo from "@/assets/icons/logos/logo_small.svg?react";
 import * as S from "./GuestList.styles";
 
-interface GuestListProps {
+/** 새로 도착한 방명록. 오로라 효과 보더와 함께 알림 UI가 노출됩니다. */
+interface NewGuestListProps {
+  isNew: true;
+}
+
+/** 일반 방명록 카드 */
+interface RegularGuestListProps {
+  isNew?: false;
   /** 방명록 작성자 이름 */
   title: string;
-  /** 새로 도착한 방명록 여부. true이면 오로라 효과 보더와 함께 방명록 알림 UI가 노출됩니다. */
-  isNew?: boolean;
   /** 사진 첨부 여부. true이면 사진 아이콘을 표시합니다. */
   hasPhoto?: boolean;
   /** 방명록 카드를 클릭했을 때 실행되는 핸들러 */
   onClick: () => void;
 }
 
-const GuestList = ({
-  title,
-  isNew = false,
-  hasPhoto = false,
-}: GuestListProps) => {
-  if (isNew) {
+type GuestListProps = NewGuestListProps | RegularGuestListProps;
+
+const GuestList = (props: GuestListProps) => {
+  if (props.isNew) {
     return (
       <S.NewCard type="button" aria-label="새로 도착한 방명록">
         <S.NewCardContent>
@@ -30,8 +33,10 @@ const GuestList = ({
     );
   }
 
+  const { title, hasPhoto = false, onClick } = props;
+
   return (
-    <S.Card type="button" aria-label={`${title} 님의 방명록`}>
+    <S.Card type="button" aria-label={`${title} 님의 방명록`} onClick={onClick}>
       <S.TitleGroup>
         <S.TitleBold aria-hidden={true}>{title}</S.TitleBold>
         <S.TitleRegular aria-hidden={true}>님의 방명록</S.TitleRegular>
