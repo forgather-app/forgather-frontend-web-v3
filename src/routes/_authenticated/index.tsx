11f5bfbdaf2cf85f01b4ade32e8215349bd2b9ee
H0ngju <hongju4385@gmail.com>
2026-07-19 16:59:34 +0900

    merge: develop 인증 라우트 구조에 홈 페이지 통합

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import HomePage from "@/pages/home/HomePage";
 
-
 export const Route = createFileRoute("/_authenticated/")({
   component: HomePage,
 });
