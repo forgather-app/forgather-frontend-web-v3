487f6c731b3553ed5d30829e038dfd8d65cd9827
H0ngju <hongju4385@gmail.com>
2026-09-21 22:52:08 +0900

    fix: 작가 기능 일시 중단에 따른 앱 다운로드 유도 UI 비활성화
    시스템 점검으로 작가 기능이 일시 중단되어 App Store 다운로드 버튼과
    안내 모달 문구를 점검 안내로 임시 전환

--- src/pages/landing/components/LandingAppStoreButtons/LandingAppStoreButtons.tsx
+++ src/pages/landing/components/LandingAppStoreButtons/LandingAppStoreButtons.tsx
@@ -1,14 +1,9 @@
-import { LANDING_APP_STORE_URL } from "@/pages/landing/LandingPage.constants";
 import * as S from "./LandingAppStoreButtons.styles";
 
 const LandingAppStoreButtons = () => {
   return (
     <S.AppButtonGroup>
-      <S.AppStoreButton
-        href={LANDING_APP_STORE_URL}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <S.AppStoreButton type="button" disabled aria-disabled="true">
         APP STORE 다운로드
       </S.AppStoreButton>
       <S.PlayStoreButton type="button" disabled aria-disabled="true">
